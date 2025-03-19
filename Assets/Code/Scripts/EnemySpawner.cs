abad33177dca279a1504e5f040e4c2da5f8508f5
DheAnto <antoniomascani@gmail.com>
2025-03-19 16:08:02 +0100

    Initial commit

---
+++ Assets/Code/Scripts/EnemySpawner.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [Header("Attributes")]
+    [SerializeField] private GameObject[] enemyPrefabs;
+
+
+    [Header("Attributes")]
+    [SerializeField] private int baseEnemies = 8;
+    [SerializeField] private float enemiesPerSecond = 0.5f;
+    [SerializeField] private float timeBetweenWaves = 5.0f;
+    [SerializeField] private float difficultyScalingFactor = 0.75f;
+
+    private int currentWave = 1;
+    private float timeSinceLastSpawn;
+    private int enemiesAlive;
+    private int enemiesLeftToSpawn;
+    private void Start()
+    {
+        enemiesLeftToSpawn = baseEnemies;
+    }
+
+    private int EnemiesPerWave(){
+        return Mathf.RoundToInt(baseEnemies * Mathf.Pow(currentWave, difficultyScalingFactor));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
