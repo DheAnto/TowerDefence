0f8988738bd6464b0d77b7a1568fe92a0a56581a
DheAnto <antoniomascani@gmail.com>
2025-04-09 18:25:31 +0200

    continuoAlgoritmoMapGenerator

--- Assets/Code/Scripts/MapGenerator.cs
+++ Assets/Code/Scripts/MapGenerator.cs
@@ -1,11 +1,13 @@
-using Unity.VisualScripting;
+
+using System.Collections;
 using UnityEngine;
+
 
 public class MapGenerator : MonoBehaviour
 {
     [SerializeField] private int mapWidth, mapHeight;
     [SerializeField] private GameObject isometricTile;
-    [SerializeField] private Sprite upDownRoad, leftRightRoad, upLeftRoad, upRightRoad, leftDownRoad, rightDownRoad;
+    [SerializeField] private Sprite upDownRoad, leftRightRoad, upLeftRoad, upRightRoad, leftDownRoad, rightDownRoad, emptyTile;
     private enum currentDirection
     {
         LEFT,
@@ -17,59 +19,113 @@ public class MapGenerator : MonoBehaviour
     private currentDirection lastDirection;
     private int curX = 0;
     private int curY = 0;
+    public struct TileData {
+        public Transform transform;
+        public int ID;
+        public SpriteRenderer spriteToUse;
+
+    }
+    private TileData[,] pathMatrix;
     private int sameDirection = 0;
-    [SerializeField] private int minSameDirection = 5;
+    [SerializeField] private int minSameDirection = 3;
     private int pathCounter = 0;
 
 
-    void Awake(){
+    void Awake()
+    {
+        pathMatrix = new TileData[mapWidth, mapHeight];
         GenerateMap();
     }
 
-    void GenerateMap(){
+    void GenerateMap()
+    {
+        
         float xOffset = (mapWidth + mapHeight) / 8f;
         float yOffset = (mapWidth + mapHeight) / 8f;
-        for (int i = mapWidth; i >= 0; i--){
-            for(int j = 0; j < mapHeight; j++){
+        for (int i = mapWidth - 1; i >= 0; i--)
+        {
+            for (int j = 0; j < mapHeight; j++)
+            {
+                Debug.Log(i +"  " + j);
                 float iOffset = (i + j) / 2f;
                 float jOffset = (i - j) / 4f;
-                
+
                 GameObject tile = Instantiate(isometricTile, new Vector3(iOffset - xOffset, jOffset + yOffset, 0), Quaternion.identity);
+                pathMatrix[i, j].ID = 0;
+                pathMatrix[i, j].spriteToUse = tile.GetComponent<SpriteRenderer>();
+                pathMatrix[i, j].transform = tile.transform;
+                pathMatrix[i, j].spriteToUse.sprite = emptyTile;
             }
         }
+        StartCoroutine(GeneratePath());
     }
 
-    void GeneratePath(){
-        int[,] pathMatrix = new int[mapWidth, mapHeight];
-        int tileNumber = Random.Range(0, mapWidth);
-        pathMatrix[tileNumber, 0] = ++pathCounter;
+    IEnumerator GeneratePath()
+    {
+        
+        curX = Random.Range(0, mapWidth);
+        pathMatrix[curX, 0].ID = ++pathCounter;
+        pathMatrix[curX, curY].spriteToUse.sprite = upDownRoad;
 
-        while(curY < mapHeight -1)
+        while (curY < mapHeight - 1)
         {
             if (sameDirection < minSameDirection)
             {
-                if (curDirection == currentDirection.LEFT && (curX == 0 || pathMatrix[curX - 1, curY] != 0)|| 
-                    curDirection == currentDirection.UP && (curY == 0 || pathMatrix[curX, curY - 1] != 0) || 
-                    curDirection == currentDirection.RIGHT && (curX == mapWidth - 1 || pathMatrix[curX + 1, curY] != 0))
+                if (curDirection == currentDirection.LEFT && (curX == 0 || pathMatrix[curX - 1, curY].ID != 0) ||
+                    curDirection == currentDirection.UP && (curY == 0 || pathMatrix[curX, curY - 1].ID != 0) ||
+                    curDirection == currentDirection.RIGHT && (curX == mapWidth - 1 || pathMatrix[curX + 1, curY].ID != 0))
                 {
                     changeDirection();
                 }
             }
+            else if (Random.Range(0, 3) == 0)
+            {
+                changeDirection();
+            }
+            yield return new WaitForSeconds(0.05f);
         }
+        
     }
 
     private void changeDirection()
     {
-        currentDirection tempDirection= (currentDirection)Random.Range(0, 4);
+        if(curDirection == currentDirection.UP){
+            
+        }
+        currentDirection tempDirection = (currentDirection)Random.Range(0, 4);
         if (tempDirection == curDirection ||
-            (tempDirection == currentDirection.LEFT && curX < minSameDirection) || 
+            (tempDirection == currentDirection.LEFT && curX < minSameDirection) ||
             (tempDirection == currentDirection.UP && curY < minSameDirection) ||
             (tempDirection == currentDirection.RIGHT && curX > mapWidth - 1 - minSameDirection))
         {
             changeDirection();
+            return;
         }
         lastDirection = curDirection;
         curDirection = tempDirection;
-        
+        sameDirection = 0;
+    }
+
+    void updateMatrix(){
+        sameDirection++;
+        switch(curDirection){
+            case (currentDirection.DOWN):
+            pathMatrix[curX, ++curY].ID = ++pathCounter;
+            break;
+            case currentDirection.UP:
+            pathMatrix[curX, --curY].ID = ++pathCounter;
+            break;
+            case currentDirection.LEFT:
+            pathMatrix[--curX, curY].ID = ++pathCounter;
+            break;
+            case currentDirection.RIGHT:
+            pathMatrix[++curX, curY].ID = ++pathCounter;
+            break;
+        }
+        pathMatrix[curX, curY].spriteToUse.sprite = upDownRoad;
+    }
+
+    void buildPath(){
+
     }
 }
